bd8dbb0c4dff635d84f746da5a40a93973b250a1
Dong Zhenpeng <328459240@qq.com>
2024-07-18 20:52:00 +0800

    1

---
+++ FIT5032_MyCSS/FIT5032_MyCSS/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_MyCSS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        //调用exmple
+        public ActionResult Example()
+        {
+            return View();
+        }
+    }
+}
